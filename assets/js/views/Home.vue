265fcfbc23fd12637f997d35d53ec655177a3c35
jonasgadeikis <jonasgadeikis.github@gmail.com>
2020-01-13 19:32:51 +0200

    Initial commit

--- assets/js/views/Home.vue
+++ assets/js/views/Home.vue
@@ -1,10 +1,64 @@
 <template>
-    <h1>Welcome to Vue!</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm">
+                <div class="input-group my-3">
+                    <input type="text" class="form-control mx-3" placeholder="API key" v-model="apiKey">
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="input-group my-3">
+                    <input type="text" class="form-control" placeholder="City" v-model="city">
+                    <div class="input-group-append">
+                        <button class="btn btn-success" type="button" @click="getWeather(apiKey, city)">
+                            <i class="fas fa-check" />
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm">
+                <ul class="nav nav-tabs">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Active</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+    import { mapActions } from 'vuex';
+
     export default {
-        name: "Home"
+        name: "Home",
+
+        data() {
+            return {
+                city: '',
+                apiKey: '',
+            }
+        },
+
+        methods: {
+            ...mapActions([
+                "getData",
+            ]),
+
+            getWeather(apiKey, city) {
+                const payload = {
+                    apiKey,
+                    city,
+                };
+
+                this.getData(payload);
+            },
+        },
     }
 </script>
 
